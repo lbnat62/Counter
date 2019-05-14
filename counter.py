ee9f4a67d51fbc320a23f77f7bf2804ed3497ce8
Nick Truex <ntruex10@gmail.com>
2019-05-14 13:59:56 -0700

    Counter Buttons

---
+++ counter.py
@@ -0,0 +1,12 @@
+class Counter:
+
+    def getValue(self):
+        return self._value
+
+    def click(self):
+        self._value = self._value + 1
+
+    def reset(self):
+        self._value = 0
+
+    
